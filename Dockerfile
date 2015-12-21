cc37cf736a472e39374bf9c3cf927aa18fd433d1
flrngel <flrngel@gmail.com>
2015-12-22 02:40:58 +0900

    MYSQL plugin with Envrionment Variable

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ RUN apt-get update && apt-get install -yq curl git unzip
 RUN cd / && curl -sSL http://emqtt.io/downloads/ubuntu -o emqttd.zip && unzip emqttd.zip && rm -rf emqttd.zip
 
 WORKDIR /emqttd
+
 RUN cd /emqttd
+
+ADD ./etc/plugin.config /emqttd/plugins/emqttd_plugin_mysql/etc/plugin.config
 ADD ./run.sh /emqttd/run.sh
 CMD ["/emqttd/run.sh"]
--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-./bin/emqttd console
+
+sudo /bin/sed -i "s/MYSQL_HOST/$MYSQL_HOST/g" /emqttd/plugins/emqttd_plugin_mysql/etc/plugin.config
+sudo /bin/sed -i "s/MYSQL_PORT/$MYSQL_PORT/g" /emqttd/plugins/emqttd_plugin_mysql/etc/plugin.config
+sudo /bin/sed -i "s/MYSQL_USER/$MYSQL_USER/g" /emqttd/plugins/emqttd_plugin_mysql/etc/plugin.config
+sudo /bin/sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" /emqttd/plugins/emqttd_plugin_mysql/etc/plugin.config
+sudo /bin/sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g" /emqttd/plugins/emqttd_plugin_mysql/etc/plugin.config
+
+./bin/emqttd start
+./bin/emqttd_ctl plugins load emqttd_plugin_mysql
+
+bash -c
